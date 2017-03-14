9c090d449c1df03ffcc8eeada4b57b49adffa6e8
Luis Cruz <luis.cruz@b2wdigital.com>
2017-03-14 02:26:51 -0300

    Enables Firebase Crash Reporting and Project multidexing

---
+++ AndroidLabs/android-labs/src/main/java/br/com/mitziin/androidlabs/AndroidLabApplication.java
@@ -0,0 +1,19 @@
+package br.com.mitziin.androidlabs;
+
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by luis.cruz on 14/03/17.
+ */
+
+public class AndroidLabApplication extends MultiDexApplication {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+}
--- AndroidLabs/android-labs/src/main/java/br/com/mitziin/androidlabs/MainActivity.java
+++ AndroidLabs/android-labs/src/main/java/br/com/mitziin/androidlabs/MainActivity.java
@@ -3,11 +3,14 @@ package br.com.mitziin.androidlabs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.crash.FirebaseCrash;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseCrash.log("Launch Main Activity!");
         setContentView(R.layout.activity_main);
     }
 }
